1e5a3d38121b9559837c93fed62291ec5196f56c
Soroush Sherafat <sorousherafat@gmail.com>
2023-04-28 20:28:43 +0330

    replaced test file and assert file with tests directory

--- vjudge.c
+++ vjudge.c
@@ -1,50 +1,56 @@
+#include <dirent.h>
+#include <getopt.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <getopt.h>
 
-static char *test_file_path;
-static char *assert_file_path;
+typedef uint64_t timestamp_t;
+
+typedef struct {
+    char *signal_name;
+    char signal_value;
+    timestamp_t timestamp;
+} assertion_t;
+
+static DIR *tests_dir;
+static assertion_t *assertions;
 
 static const char* usage =
     "Usage: vjudge [OPTIONS]... FILE...\n"
-    "A command-line tool for automatic judge of verilog HDL code.\n\n"
+    "A command-line tool for automatic judge of verilog HDL code.\n"
+    "\n"
     "Positional arguments:\n"
-    "  FILE...               Paths to source files.\n\n"
+    "  FILE...               Paths to source files.\n"
+    "\n"
     "Optional arguments:\n"
-    "  -t, --test FILE       Path to the test file (required).\n"
-    "  -a, --assert FILE     Path to the assert file (required).\n"
+    "  -t, --test DIR        Path to the tests directory, containing testbenches and assertions (required).\n"
     "  -v, --verbose         Print verbose output.\n"
     "  -h, --help            Print this help message and exit.\n";
 
 struct option options[] = {
-    {"test", required_argument, 0, 't'},
-    {"assert", required_argument, 0, 'a'},
-    {"verbose", no_argument, 0, 'v'},
-    {"help", no_argument, 0, 'h'},
-    {0, 0, 0, 0}
+    {"test",    required_argument, 0, 't'},
+    {"verbose", no_argument,       0, 'v'},
+    {"help",    no_argument,       0, 'h'},
+    {0,         0,                 0,   0}
 };
 
 
 int main(int argc, char *argv[])
 {    
     bool test_flag = false;
-    bool assert_flag = false;
     bool verbose_flag = false;
+    char *tests_dir_path;
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "t:a:vh", options, NULL)) != -1)
+    while ((opt = getopt_long(argc, argv, "t:vh", options, NULL)) != -1)
     {
         switch (opt)
         {
             case 't':
                 test_flag = true;
-                test_file_path = optarg;
-                break;
-            case 'a':
-                assert_flag = true;
-                assert_file_path = optarg;
+                tests_dir_path = optarg;
                 break;
             case 'v':
                 verbose_flag = true;
@@ -67,13 +73,7 @@ int main(int argc, char *argv[])
 
     if (!test_flag)
     {
-        fprintf(stderr, "Test file not provided\n");
-        args_provided = false;
-    }
-
-    if (!assert_flag)
-    {
-        fprintf(stderr, "Assert file not provided\n");
+        fprintf(stderr, "Test directory not provided\n");
         args_provided = false;
     }
 
@@ -92,22 +92,18 @@ int main(int argc, char *argv[])
     /* Check if the given files exist. */
     bool files_exist = true;
 
-    if (access(test_file_path, F_OK) == -1)
+    if ((tests_dir = opendir(tests_dir_path)) == NULL)
     {
-        fprintf(stderr, "Test file does not exist: %s\n", test_file_path);
-        files_exist = false;
-    }
-    
-    if (access(assert_file_path, F_OK) == -1)
-    {
-        fprintf(stderr, "Assert file does not exist: %s\n", assert_file_path);
+        fprintf(stderr, "Could not open tests directory: %s\n", tests_dir_path);
         files_exist = false;
     }
 
+    /* TODO: Add checks for files in `tests_dir`. */
+    
     for (int i = optind; i < argc; i++)
-        if (access(argv[i], F_OK) == -1)
+        if (access(argv[i], R_OK) == -1)
         {
-            fprintf(stderr, "Source file does not exist: %s\n", argv[i]);
+            fprintf(stderr, "Could not open source file: %s\n", argv[i]);
             files_exist = false;
         }
 
@@ -117,11 +113,9 @@ int main(int argc, char *argv[])
 
     if (verbose_flag)
     {
-        printf("\n");
-        printf("Test file path:   %s\n", test_file_path);
-        printf("Assert file path: %s\n", assert_file_path);
+        printf("Tests directory path:   %s\n", tests_dir_path);
         for (int i = optind; i < argc; i++)
-            printf("Source file path: %s\n", argv[i]);
-        printf("\n");
+            printf("Source file path:       %s\n", argv[i]);
     }
 }
+
