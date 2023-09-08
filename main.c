c7601423a2ad6e4b4fb8299e98846b38296c8ab7
Soroush Sherafat <sorousherafat@gmail.com>
2023-09-08 16:33:26 +0330

    fotmat using `clang-format`

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
+#include "libvcd.h"
+#include <dirent.h>
+#include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <getopt.h>
-#include "libvcd.h"
 
 #define MAX_NAME_SIZE 128
 #define MAX_ASSERTIONS_NO 1024
@@ -30,23 +30,19 @@ typedef struct {
 } test_t;
 
 static const char *usage =
-        "Usage: vjudge [OPTIONS]... FILE...\n"
-        "A command-line tool for automatic judge of verilog HDL code.\n"
-        "\n"
-        "Positional arguments:\n"
-        "  FILE...               Paths to source files.\n"
-        "\n"
-        "Optional arguments:\n"
-        "  -t, --test DIR        Path to the tests directory, containing testbenches and assertions (required).\n"
-        "  -v, --verbose         Print verbose output.\n"
-        "  -h, --help            Print this help message and exit.\n";
+    "Usage: vjudge [OPTIONS]... FILE...\n"
+    "A command-line tool for automatic judge of verilog HDL code.\n"
+    "\n"
+    "Positional arguments:\n"
+    "  FILE...               Paths to source files.\n"
+    "\n"
+    "Optional arguments:\n"
+    "  -t, --test DIR        Path to the tests directory, containing testbenches and assertions (required).\n"
+    "  -v, --verbose         Print verbose output.\n"
+    "  -h, --help            Print this help message and exit.\n";
 
 static const struct option options[] = {
-        {"test", required_argument, 0, 't'},
-        {"verbose", no_argument,    0, 'v'},
-        {"help", no_argument,       0, 'h'},
-        {0, 0,                      0, 0}
-};
+    {"test", required_argument, 0, 't'}, {"verbose", no_argument, 0, 'v'}, {"help", no_argument, 0, 'h'}, {0, 0, 0, 0}};
 
 static const char *out_file_name = ".tmp.o";
 static const char *vcd_file_name = ".tmp.vcd";
@@ -57,23 +53,23 @@ static char *tests_dir_path;
 static test_t tests[MAX_TESTS_NO];
 static size_t tests_count = 0;
 
-static void check_args_provided(int argc);
+void check_args_provided(int argc);
 
-static void check_files_existence(int argc, char *const *argv);
+void check_files_existence(int argc, char *const *argv);
 
-static void print_input_if_verbose(int argc, char *const *argv);
+void print_input_if_verbose(int argc, char *const *argv);
 
-static void parse_input(int argc, char *argv[]);
+void parse_input(int argc, char *argv[]);
 
-static bool try_get_test_name(char *file_name, char **test_name);
+bool try_get_test_name(char *file_name, char **test_name);
 
-static FILE *open_assertion_file(const struct dirent *tests_dirent, const char *assertion_file_path);
+FILE *open_assertion_file(const struct dirent *tests_dirent, const char *assertion_file_path);
 
-static void write_assertion_file_path(char *assertion_file_path, const char *test_name);
+void write_assertion_file_path(char *assertion_file_path, const char *test_name);
 
-static void read_assertions(test_t *test, FILE *assertion_file);
+void read_assertions(test_t *test, FILE *assertion_file);
 
-static void write_assertion(test_t *test, timestamp_t timestamp, char *expected_value, char *signal_name);
+void write_assertion(test_t *test, timestamp_t timestamp, char *expected_value, char *signal_name);
 
 void load_tests();
 
@@ -103,8 +99,8 @@ void print_failed_assertions_if_verbose(const char *src_file_path, const test_t 
 
 bool check_assertion(vcd_t *vcd, assertion_result_t *assertion_result, assertion_t *assertion);
 
-void
-print_assertion_if_failed(const char *src_file_path, const test_t *test, const assertion_result_t *assertion_result);
+void print_assertion_if_failed(const char *src_file_path, const test_t *test,
+                               const assertion_result_t *assertion_result);
 
 test_t *read_test(char *test_name);
 
@@ -137,8 +133,7 @@ void test_src_file(const char *src_file_path) {
     print_src_report(src_file_path, passed_tests_count);
 }
 
-void
-print_src_report(const char *src_file_path, size_t passed_tests_count) {
+void print_src_report(const char *src_file_path, size_t passed_tests_count) {
     printf("src file '%s': %zu/%zu tests passed\n\n", src_file_path, passed_tests_count, tests_count);
 }
 
@@ -168,7 +163,7 @@ bool check_assertion(vcd_t *vcd, assertion_result_t *assertion_result, assertion
 bool check_assertions(const test_t *test, vcd_t *vcd) {
     bool test_passed = true;
     for (int assertion_index = 0; assertion_index < test->assertions_count; ++assertion_index) {
-        assertion_result_t *assertion_result = (assertion_result_t *) &test->assertion_results[assertion_index];
+        assertion_result_t *assertion_result = (assertion_result_t *)&test->assertion_results[assertion_index];
         assertion_t *assertion = &assertion_result->assertion;
         test_passed &= check_assertion(vcd, assertion_result, assertion);
     }
@@ -178,7 +173,7 @@ bool check_assertions(const test_t *test, vcd_t *vcd) {
 bool run_test(const char *src_file_path, test_t *test) {
     create_vcd_file(src_file_path, test);
 
-    vcd_t *vcd = open_vcd((char *) vcd_file_name);
+    vcd_t *vcd = open_vcd((char *)vcd_file_name);
     if (vcd == NULL) {
         fprintf(stderr, "An error occurred when opening .vcd file '%s'", vcd_file_name);
         exit(EXIT_FAILURE);
@@ -197,7 +192,8 @@ bool run_test(const char *src_file_path, test_t *test) {
 }
 
 void print_failed_assertions_if_verbose(const char *src_file_path, const test_t *test) {
-    if (!verbose_flag) return;
+    if (!verbose_flag)
+        return;
 
     for (int assertion_index = 0; assertion_index < test->assertions_count; ++assertion_index) {
         const assertion_result_t *assertion_result = &test->assertion_results[assertion_index];
@@ -205,12 +201,11 @@ void print_failed_assertions_if_verbose(const char *src_file_path, const test_t 
     }
 }
 
-void
-print_assertion_if_failed(const char *src_file_path, const test_t *test, const assertion_result_t *assertion_result) {
+void print_assertion_if_failed(const char *src_file_path, const test_t *test,
+                               const assertion_result_t *assertion_result) {
     if (!assertion_result->passed) {
-        printf("Assertion failed on test '%s' for src file '%s': Expected '%s' but got '%s'\n",
-               test->name, src_file_path, assertion_result->assertion.expected_value,
-               assertion_result->actual_value);
+        printf("Assertion failed on test '%s' for src file '%s': Expected '%s' but got '%s'\n", test->name,
+               src_file_path, assertion_result->assertion.expected_value, assertion_result->actual_value);
     }
 }
 
@@ -265,7 +260,7 @@ bool test_exists(const char *test_name) {
 }
 
 void read_test_assertions(const struct dirent *tests_dirent, const char *test_name, test_t *test) {
-    char *assertion_file_path = (char *) malloc(PATH_MAX * sizeof(char));
+    char *assertion_file_path = (char *)malloc(PATH_MAX * sizeof(char));
     write_assertion_file_path(assertion_file_path, test_name);
 
     FILE *assertion_file = open_assertion_file(tests_dirent, assertion_file_path);
@@ -315,24 +310,24 @@ void parse_input(int argc, char *argv[]) {
     int opt;
     while ((opt = getopt_long(argc, argv, "t:vh", options, NULL)) != -1) {
         switch (opt) {
-            case 't':
-                test_flag = true;
-                tests_dir_path = optarg;
-                break;
-            case 'v':
-                verbose_flag = true;
-                break;
-            case 'h':
-                printf("%s", usage);
-                exit(EXIT_SUCCESS);
-            case '?':
-                fprintf(stderr, "Unknown option or missing argument: %s\n%s", argv[optind - 1], usage);
-                exit(EXIT_FAILURE);
-            case ':':
-                fprintf(stderr, "Missing argument for option: %s\n%s", argv[optind - 1], usage);
-                exit(EXIT_FAILURE);
-            default:
-                exit(EXIT_FAILURE);
+        case 't':
+            test_flag = true;
+            tests_dir_path = optarg;
+            break;
+        case 'v':
+            verbose_flag = true;
+            break;
+        case 'h':
+            printf("%s", usage);
+            exit(EXIT_SUCCESS);
+        case '?':
+            fprintf(stderr, "Unknown option or missing argument: %s\n%s", argv[optind - 1], usage);
+            exit(EXIT_FAILURE);
+        case ':':
+            fprintf(stderr, "Missing argument for option: %s\n%s", argv[optind - 1], usage);
+            exit(EXIT_FAILURE);
+        default:
+            exit(EXIT_FAILURE);
         }
     }
 }
